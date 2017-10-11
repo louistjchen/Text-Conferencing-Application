580c54cccac4fc5f0032e68c33ed91686dbbe460
Kevin Chang <kevinkc.chang@mail.utoronto.ca>
2017-10-11 19:45:26 -0400

    Section 2 and Section 3 (1st Draft)

--- lab1/deliver.c
+++ lab1/deliver.c
@@ -6,77 +6,157 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <math.h>
 
-#define BUFSIZE 100 /* set incoming message size to max 100 Bytes */
+/* FUNCTION BEGIN - initialize socket */
+int initialize_socket(struct sockaddr_in * server_addr_info, char * server_ip_addr, unsigned short port_number) {
 
+    /* build server's IP address */
+    server_addr_info->sin_family = AF_INET; /* for IPv4 */
+    server_addr_info->sin_port = htons(port_number);
+    
+    inet_pton(AF_INET, server_ip_addr, (struct in_addr *) &(server_addr_info->sin_addr));
+
+    /* open a socket */
+    int sock_fd = -1;
+    while (sock_fd < 0) {
+        sock_fd = socket(AF_INET, SOCK_DGRAM, 0); /* domain, socket type, protocol */    
+    }
+    return sock_fd;
+}
+/* FUNCTION END - initialize socket */
+
+/* FUNCTION BEGIN - main */
 int main(int argc, char * argv[]) {
 	
-	/* declare variables */
-	unsigned short port_number = 22000; /* default to 22000 */
-	int client_fd = -1;
-	struct sockaddr_in server_addr;
-	char * server_in_addr = "127.0.0.1";
-	char buf[BUFSIZE];
+	/* section 1 - BEGIN */
+    /* initialize address info variables */
+	unsigned short port_number = 22000; 
+	int sock_fd = -1;
+	struct sockaddr_in server_addr_info;
+    int server_len = sizeof(server_addr_info);
+	char * server_ip_addr = "127.0.0.1";
 
-
-	
 	/* pre-processing */
-	bzero(&server_addr, sizeof(server_addr));
-	
+	bzero(&server_addr_info, sizeof(server_addr_info));
 	if (argc == 3) {
-		server_in_addr = argv[1];
+		server_ip_addr = argv[1];
 		port_number = atoi(argv[2]);
 	}
 
-	/* build server's IP address */
-	server_addr.sin_family = AF_INET; /* for IPv4 */
-	server_addr.sin_port = htons(port_number);
-	
-	inet_pton(AF_INET, server_in_addr, (struct in_addr *) &(server_addr.sin_addr));
-	/* open a socket */
-	while (client_fd < 0) {
-		client_fd = socket(AF_INET, SOCK_DGRAM, 0); /* domain, socket type, protocol; use SOCK_STREAM to open TCP socket, SOCK_DGRAM to open UDP socket */
-	}
-	// printf("Opened socket at %d\n", client_fd);
+    /* initialize socket */
+    sock_fd = initialize_socket(&server_addr_info, server_ip_addr, port_number);
+    printf("Initialize socket %d\n",sock_fd);
 	
 	/* ask user to input file name */
-	char transport_type[10];
-	char transport_path[20];
+	char transport_type[1000];
+	char transport_file_name[1000];
 	FILE * fp = NULL;
 	printf("Please input <transport protocol type> <transport file path>: ");
 	scanf("%s", transport_type);
-	scanf("%s", transport_path);
+	scanf("%s", transport_file_name);
 	if(strcmp(transport_type, "ftp") != 0) {
 		printf("Please check transport protocol type and restart the program.\n");
 		return 0;
 	}
-	if(fopen(transport_path, "r") == NULL) {
+    FILE * file_pointer = fopen(transport_file_name, "r");
+	if(file_pointer == NULL) {
 		printf("Entered file path is invalid. Please confirm and restart the program.\n");
 		return 0;
-	}
+	} 
+    /* section 1 - END */    
 
-	/* send message to server */
-	int send_success = -1;
-	char msg[] = "ftp";
-	int server_len = sizeof(server_addr);
-	while (send_success < 0) {
-		send_success = sendto(client_fd, msg, strlen(msg), 0, (struct sockaddr *) &server_addr, server_len);
-	}
+    /* section 3 - BEGIN */
+    /* determine file size (total # bytes) */
+    struct stat st;
+    unsigned int file_size = 0;
+    if (!stat(transport_file_name,&st)) {
+        file_size = st.st_size;
+    }
+    /* dynamically allocate space for char array to load all the data into */
+    char * filedata = malloc(sizeof(char) * file_size);
+    /* load entire file into char array above */
+    memcpy(filedata, file_pointer, file_size);
+    /* determine number of packets */
+    int last_packet_size = file_size % 1000;
+    int num_packets = (last_packet_size) ? ((file_size/1000) + 1) : (file_size/1000);
+    printf("Num packets:%u Last packet size:%u\n",num_packets,last_packet_size);
+	/* initialize all outgoing packets */
+    char ** outgoing_packet = malloc(sizeof(char *) * num_packets);
+    int i, j, packet_size;
+    for (i = 0; i < num_packets; i++) {
+        packet_size = (i == num_packets - 1) ? last_packet_size : 1000;
+        outgoing_packet[i] = (char *)malloc(sizeof(char) * 1100);
+        j = sprintf(outgoing_packet[i],"%u:%u:%u:%s:%s",num_packets,i,packet_size,transport_file_name,filedata + (i*1000));
+    }
+    unsigned int current_packet = 0; /* counter to track current pending packet */
+    // while (packet_sent_number < num_packets) {
+    //     /* initialize outgoing packet data structure */
+    //     initialize_packet(&outgoing_packet,num_packets,current_packet,transport_file_name);
+    //     /* populate packet with specific info */
+    //     populate_packet(&outgoing_packet,packet_sent_number,last_packet_size,data_packets[packet_sent_number]);
+    //     /* reset incoming ack data structure */
+    //     bzero(&incoming_ack,sizeof(incoming_ack));
+    //     /* initialize ack boolean flag to false */
+    //     int ack_flag = 0;
+    //     do {
+    //         /* first send the packet once */
+    //         printf("Send packet %d\n",packet_sent_number);
+    //         sendto(sock_fd,&outgoing_packet,sizeof(outgoing_packet),0,(struct sockaddr *) &server_addr_info,server_len);
+    //         /* check for incoming ack/nack */
+    //         printf("Wait on ack \n");
+    //         int recv_success = recvfrom(sock_fd,&incoming_ack,sizeof(incoming_ack),0,(struct sockaddr *) &server_addr_info,&server_len);
+    //         if (recv_success >= 0) {
+    //             printf("Received ack from server\n");
+    //             /* if incoming ack frag no. matches packet and incoming ack status is true, increment packet_sent_number */
+    //             if (incoming_ack.frag_no == packet_sent_number && incoming_ack.status) {
+    //                 ack_flag = 1;
+    //                 packet_sent_number++;
+    //             }
+    //         }
+    //         /* reset incoming ack data structure before next iteration */
+    //         bzero(&incoming_ack,sizeof(incoming_ack));
+    //     } while (!ack_flag);        
+    // }
+
+
+    // struct timeval start, stop; /* initialize variables to measure elapsed time */
+
+    // int send_success = -1;
+	// char msg[] = "ftp";
+	// int server_len = sizeof(server_addr_info);
+	// gettimeofday(&start, NULL); /* start time */
+	// while (send_success < 0) {
+	// 	send_success = sendto(sock_fd, msg, strlen(msg), 0, (struct sockaddr *) &server_addr_info, server_len);
+	// }
 	// printf("Sent message\n");
 
 	/* print server's reply */
-	int recv_success = -1;
-	while (recv_success < 0) {
-		bzero(buf, BUFSIZE);
-		recv_success = recvfrom(client_fd, buf, BUFSIZE, 0, (struct sockaddr *) &server_addr, &server_len);
-	}
-	// printf("Received message\n");
+	// int recv_success = -1;
+	// while (recv_success < 0) {
+	// 	bzero(buf, BUFSIZE);
+	// 	recv_success = recvfrom(sock_fd, buf, BUFSIZE, 0, (struct sockaddr *) &server_addr_info, &server_len);
+	// }
+	// gettimeofday(&stop, NULL); /* stop time */
+	// printf("RTT (milliseconds): %f\n", (float)(stop.tv_usec - start.tv_usec)/1000);
+	// // printf("Received message\n");
 
-	if (strstr(buf,"yes") != NULL) {
-		printf("A file transfer can start\n");
-	} else {
-		printf("Failed :\(\n");
-	}
+	// if (strstr(buf,"yes") != NULL) {
+	// 	printf("A file transfer can start\n");
+	// } else {
+	// 	printf("Failed :\(\n");
+	// }
+    /* section 3 - END */
 
+    // /* close socket */
+    // close(sock_fd);
+    // /* free dynamically allocated memory */
+    // for (i = 0; i < num_packets; i++) {
+    //     free(data_packets[i]);
+    // }
+    // free(data_packets);
 	return 0;
 }
+/* FUNCTION END - main */
--- lab1/server.c
+++ lab1/server.c
@@ -6,63 +6,139 @@
 #include <unistd.h>
 #include <stdio.h>
 
-#define BUFSIZE 100 /* set incoming message size to max 100 Bytes */
+/* DEFINE BEGIN - packet struct */
+typedef struct packet {
+    unsigned int total_frag;
+    unsigned int frag_no;
+    unsigned int size;
+    char* filename;
+    char filedata[1000];
+} packet;
+/* DEFINE END - packet struct */
 
+/* DEFINE BEGIN - ack struct */
+typedef struct ack {
+    unsigned int status;
+    unsigned int frag_no;
+} ack;
+/* DEFINE END - ack struct */
+
+/* FUNCTION BEGIN - initialize + bind socket */
+int initialize_bind_socket(struct sockaddr_in * server_addr_info, unsigned short port_number) {
+    
+    /* build server's IP address */
+    server_addr_info->sin_family = AF_INET; /* for IPv4 */
+    server_addr_info->sin_addr.s_addr = htonl(INADDR_ANY); /* IP address can be anything */
+    server_addr_info->sin_port = htons(port_number); /* user-input port number */
+
+    /* open a socket */
+    int sock_fd = -1;
+    while (sock_fd < 0) {
+        sock_fd = socket(AF_INET, SOCK_DGRAM, 0); /* domain, socket type, protocol */
+    }
+    int bind_success = -1;
+    while (bind_success < 0) {
+        bind_success = bind(sock_fd, (struct sockaddr *) server_addr_info, sizeof(*server_addr_info));
+    }
+    printf("Bind at socket %u\n",sock_fd);
+    return sock_fd;
+}
+/* FUNCTION BEGIN - initialize + bind socket */
+
+/* FUNCTION BEGIN - initialize + populate ack */
+void initialize_populate_ack(ack * outgoing_ack, unsigned int frag_no) {
+    bzero(outgoing_ack,sizeof(*outgoing_ack));
+    outgoing_ack->status = 0;
+    outgoing_ack->frag_no = frag_no;
+    return;
+}
+/* FUNCTION END - initialize ack */
+
+/* FUNCTION BEGIN - copy data */
+void copy_data(char * data_packet, packet * incoming_packet) {
+    unsigned int i;
+    for (i = 0; i < incoming_packet->size; i++) {
+        data_packet[i] = incoming_packet->filedata[i];
+    }
+    return;
+}
+
+/* FUNCTION END - copy data */
+
+/* FUNCTION BEGIN - main */
 int main(int argc, char * argv[]) {
 	
-	/* declare variables */
+	/* section 1 - BEGIN */
+    /* initialize address info variables */
 	unsigned short port_number = 22000; /* default to 22000 */
-	int server_fd = -1;
-	struct sockaddr_in server_addr, anyclient_addr;
-	socklen_t addrlen;
-	char buf[BUFSIZE]; 
+	int sock_fd = -1;
+	struct sockaddr_in server_addr_info, anyclient_addr_info;
+	unsigned int anyclient_len;
 
 	/* pre-processing */
-	bzero(&server_addr, sizeof(server_addr)); /* clear server_addr */
-
+	bzero(&server_addr_info, sizeof(server_addr_info)); 
 	if (argc == 2) {
 		port_number = atoi(argv[1]);
 	}
 
-	/* build server's IP address */
-	server_addr.sin_family = AF_INET; /* for IPv4 */
-	server_addr.sin_addr.s_addr = htonl(INADDR_ANY); /* IP address can be anything, as client will have the same IP address */
-	server_addr.sin_port = htons(port_number); /* use user-input port number */
+    /* initialize + bind socket */
+    sock_fd = initialize_bind_socket(&server_addr_info, port_number);
+    /* section 1 - END */
 
-	/* open a socket and bind to local interface */
-	while (server_fd < 0) {
-		server_fd = socket(AF_INET, SOCK_DGRAM, 0); /* domain, socket type, protocol; use SOCK_STREAM to open TCP socket, SOCK_DGRAM to open UDP socket */
-	}
-	// printf("Opened socket at %d\n", server_fd);
-	int bind_success = -1;
-	while (bind_success < 0) {
-		bind_success = bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)); /* socket file descriptor, pointer to sockaddr, size of sockaddr */
-	}
-	// printf("Bind socket\n");
-
-	/* now receive incoming connectionless packets */
-	int recv_success = -1;
-	while (recv_success < 0) {
-		bzero(buf, BUFSIZE);
-		recv_success = recvfrom(server_fd, buf, BUFSIZE, 0, (struct sockaddr *) &anyclient_addr, &addrlen); /* socket, buffer,  buffer length, flags, pointer to sockaddr, size of sockaddr */		
-	}
-	// printf("Received message\n");
-
-	char * reply;
-	if (strstr(buf,"ftp") != NULL) {
-		reply = "yes";
-	}
-	else {
-		reply = "no";
-	}
-	
-	int send_success = -1;
-	while (send_success < 0) {
-		send_success = sendto(server_fd, reply, strlen(reply), 0, (struct sockaddr *) &anyclient_addr, addrlen);
-	}
-	// printf("Send message\n");
+    /* section 3 - BEGIN */	
+	unsigned int packet_recv_number = 0; /* data structure to track the next fragment number to receive */
+    unsigned int num_packets = 100000000; /* initialize to arbitrarily high number */
+    packet incoming_packet; /* data structure to receive incoming packets */
+    ack outgoing_ack; /* data structure to send outgoing acks */
+    char ** data_packets;
+    while (packet_recv_number <= num_packets) {
+        /* reset outgoing ack data structure */
+        initialize_populate_ack(&outgoing_ack,packet_recv_number);
+        /* reset incoming packet data structure */
+		bzero(&incoming_packet, sizeof(incoming_packet));
+		do {
+            /* send ack/nack */
+        } while (recvfrom(sock_fd,&incoming_packet,sizeof(incoming_packet),MSG_DONTWAIT,(struct sockaddr *) &anyclient_addr_info,&anyclient_len) < 0);
 
 
-	close(server_fd);
+        int recv_success = recvfrom(sock_fd,&incoming_packet,sizeof(incoming_packet),0,(struct sockaddr *) &anyclient_addr_info,&anyclient_len);
+	    if (recv_success >= 0) {
+            printf("Received packet %u with total frag %u\n",incoming_packet.frag_no,incoming_packet.total_frag);
+            /* update num_packets once a packet is received, and allocate memory accordingly */
+            if (incoming_packet.total_frag < num_packets) {
+                num_packets = incoming_packet.total_frag;
+                printf("Updated num packets: %u\n",num_packets);
+                data_packets = malloc(sizeof(char *) * num_packets);
+                unsigned int i;
+                for (i = 0; i < num_packets; i++) {
+                    data_packets[i] = (char *) malloc(sizeof(char) * 1000 + 1);
+                }
+            }
+            /* if incoming packet frag no. matches packet, copy data, set outgoing ack status to true, and increment packet_recv_number */
+            if (incoming_packet.frag_no == packet_recv_number) {
+                copy_data(data_packets[packet_recv_number],&incoming_packet);
+                printf("Copied data\n");
+                outgoing_ack.status = 1;
+                packet_recv_number++;
+            } else if (incoming_packet.frag_no < packet_recv_number) {
+                outgoing_ack.status = 1;
+                outgoing_ack.frag_no = incoming_packet.frag_no;
+            }
+        }
+        /* send ack/nack */
+        sendto(sock_fd,&outgoing_ack,sizeof(outgoing_ack),0,(struct sockaddr *) &anyclient_addr_info,anyclient_len);
+        printf("Sent ack to client\n");
+    }
+    /* section 3 - END */
+
+	/* close socket */
+    close(sock_fd);
+    /* free dynamically allocated memory */
+    unsigned int i;
+    for (i = 0; i < num_packets; i++) {
+        free(data_packets[i]);
+    }
+    free(data_packets);
 	return 0;
-}	
+}
+/* FUNCTION END - main */	
